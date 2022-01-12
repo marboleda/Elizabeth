1a0d9ac1003c58a596b8739fdac6a7ce7376892f
Marco <arboleda.marco@gmail.com>
2022-01-11 20:13:54 -0500

    Set up skeleton of the Navigational Bar

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+      "./sections/*.liquid",
+      "./layout/*.liquid",
+      "./snippets/*.liquid",
+      "./templates/*.liquid",
+      "./templates/customers/*.liquid"         //If you're not using JSON for customer templates
+  ],
+  theme: {
+      extend: {},
+  },
+  plugins: [],
+}
